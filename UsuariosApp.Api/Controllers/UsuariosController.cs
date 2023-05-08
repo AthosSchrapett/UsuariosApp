f37f30824986f9fb5e3f719fc3409596b1dbc79a
aschrapett <aschrapett@infisconsultoria.com.br>
2023-05-08 10:43:57 -0300

    first commit

---
+++ UsuariosApp.Api/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        /// <summary>
+        /// Autenticação de usuários
+        /// </summary>
+        [HttpPost("autenticar")]
+        public IActionResult Autenticar()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Criação de usuários
+        /// </summary>
+        [HttpPost("criar-conta")]
+        public IActionResult CriarConta()
+        {
+            return Ok();
+        }
+    }
+}
